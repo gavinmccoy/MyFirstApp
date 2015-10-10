7d66c0c652fb3aa796e6f95ffb2730466ca2fc20
gavinmccoy <gavin.mccoy2@mail.dcu.ie>
2015-10-10 12:18:12 +0100

    Adding the Action Bar

--- app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -1,13 +1,16 @@
 package com.mycompany.myfirstapp;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-
+// developer.android.com says to use
+// public class MyActivity extends ActionBarActivity {
+// but android studio says to use
 public class MyActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
